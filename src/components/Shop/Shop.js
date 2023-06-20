8e6778cc711d859c5f3ad3e97996a319deeb6695
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-20 16:50:31 +0600

    product card done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
@@ -13,7 +14,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h1>This is all products: {products.length}</h1>
+                {
+                   products.map(product => <Product 
+                    key={product.id}
+                    product = {product}
+                    ></Product>) 
+                }
             </div>
             <div className="cart-container">
                 <h1>Order Summary</h1>
